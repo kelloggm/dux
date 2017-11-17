08b8846608267a3a6e7fcc4001adf2f5a6fcdb26
Steven Lyubomirsky <sslyu@cs.washington.edu>
2017-11-17 14:30:27 -0800

    Don't try opening/hashing input files that failed to open

--- src/org/dux/cli/DuxBuildTracer.java
+++ src/org/dux/cli/DuxBuildTracer.java
@@ -76,6 +76,11 @@ public class DuxBuildTracer {
                 continue;
             }
 
+	    // disregard if return value indicated failure
+	    if (c.returnValue == -1) {
+		continue;
+	    }
+
             // don't hash if it's already present
             if (fileHashes.containsKey(c.call)) {
                 continue;
