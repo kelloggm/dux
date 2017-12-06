c0ebe62d615a174359b19b49ad0d77d79b188db7
Martin Kellogg <kelloggm@cs.washington.edu>
2017-12-06 14:32:23 -0800

    have smoke test print expected and actual dux results

--- tst/smoke.sh
+++ tst/smoke.sh
@@ -11,3 +11,11 @@ export GOOGLE_APPLICATION_CREDENTIALS=../../credentials/GOOGLE_APPLICATION_CREDE
 ../../bazel-bin/dux -c make
 
 diff build.dux expected.dux
+
+echo "printing build.dux"
+
+../../bazel-bin/dux -f build.dux
+
+echo "printing expected.dux"
+
+../../bazel-bin/dux -f expected.dux
