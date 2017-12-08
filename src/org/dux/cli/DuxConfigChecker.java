91f17cfcd29f1425119c159c315d3a0fa5a20a1b
Steven Lyubomirsky <sslyu@cs.washington.edu>
2017-12-07 18:08:55 -0800

    Quieted down some of the log messages; unix principle of being silent when everything is working. Keep notifications when pulling files since that's a big deal

--- src/org/dux/cli/DuxConfigChecker.java
+++ src/org/dux/cli/DuxConfigChecker.java
@@ -29,7 +29,7 @@ public class DuxConfigChecker {
     public void checkConfig(DuxConfiguration config) throws IOException, FileNotFoundException {
         for (DuxConfigurationEntry entry : config) {
             // if file does not exist, try to fetch it
-            DuxCLI.logger.info("Checking if file {} exists", entry.path.toString());
+            DuxCLI.logger.debug("Checking if file {} exists", entry.path.toString());
             if (!entry.path.exists()) {
                 DuxCLI.logger.info("File {} does not exist, pulling", entry.path.toString());
                 if (!store.fetchFile(entry.hashCode.toString(),
@@ -50,7 +50,7 @@ public class DuxConfigChecker {
                 continue;
             }
 
-            DuxCLI.logger.info("{} exists and hash matches", entry.path.toString());
+            DuxCLI.logger.debug("{} exists and hash matches", entry.path.toString());
         }
     }
 }
