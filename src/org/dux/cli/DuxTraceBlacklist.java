b52438b63f59dc545c38ff08ae22acf0f2094e50
Steven Lyubomirsky <sslyu@cs.washington.edu>
2017-12-07 17:29:32 -0800

    Check if parent directories of files are in the Dux blacklist

--- src/org/dux/cli/DuxTraceBlacklist.java
+++ src/org/dux/cli/DuxTraceBlacklist.java
@@ -14,7 +14,8 @@ import java.util.HashSet;
  * An object responsible for determining what files should be excluded
  * from a Dux configuration. Contains hardcoded defaults (the user can
  * choose whether to include them) and can read additional options from
- * a file.
+ * a file. Any directory specified in the blacklist will exclude all
+ * children of the directory.
  */
 public class DuxTraceBlacklist {
     private static final String BLACKLIST_FILE_NAME = ".duxignore";
@@ -50,15 +51,24 @@ public class DuxTraceBlacklist {
 	}
     }
 
-    public boolean contains(String path) {
-	return paths.contains(Paths.get(path));
+    public boolean contains(Path path) {
+	// check if any parent directory of p is in the blacklist either;
+	// normalize so that if any parent directory is in the blacklist,
+	// this loop will catch it
+	for (Path p = path.toAbsolutePath().normalize(); p != null; p = p.getParent()) {
+	    if (paths.contains(p)) {
+		return true;
+	    }
+	}
+
+	return false;
     }
 
-    public boolean contains(Path p) {
-	return paths.contains(p);
+    public boolean contains(String path) {
+	return contains(Paths.get(path));
     }
 
     public boolean contains(File f) {
-	return paths.contains(f.toPath());
+	return contains(f.toPath());
     }
 }
