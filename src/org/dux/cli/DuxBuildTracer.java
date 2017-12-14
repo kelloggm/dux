5661c68487cc3e415472a50e42bcca682fd4e289
Martin Kellogg <kelloggm@cs.washington.edu>
2017-12-13 17:40:03 -0800

    do not save all env vars

--- src/org/dux/cli/DuxBuildTracer.java
+++ src/org/dux/cli/DuxBuildTracer.java
@@ -73,7 +73,7 @@ public class DuxBuildTracer {
                     envPaths.put(p, var.getKey());
                     DuxCLI.logger.debug("path: {} | variable: {}", p, var.getKey());
                     // As a temp measure, save all environment variables. TODO be more precise.
-                    varsToSave.add(new DuxConfigurationVar(var.getKey(), p.toString(), values.length > 1));
+                    // varsToSave.add(new DuxConfigurationVar(var.getKey(), p.toString(), values.length > 1));
                 } catch (InvalidPathException e) {
                     // an environment variable had an invalid path as its value. This is fine.
                 }
